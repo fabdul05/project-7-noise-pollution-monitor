9974caebdbd27cd06133033deb18ff3ef0c2e970
Farid Chad Abdulsamad <chad.samad@gmail.com>
2025-12-09 22:44:22 -0500

    Enhance SoundMonitor with WiFi and server capabilities
    Added WiFi and WebServer support for handling sound events.

--- SoundMonitor.h
+++ SoundMonitor.h
@@ -2,18 +2,27 @@
 #define SOUNDMONITOR_H
 
 #include <Arduino.h>
+#include <WiFi.h>
+#include <WebServer.h>
 
 class SoundMonitor {
 public:
     SoundMonitor(int pin, int threshold);
 
-    void begin();
-    void update();  // call repeatedly inside loop()
+    void begin(const char* ssid, const char* password);
+    void update();
 
 private:
     int soundPin;
     int threshold;
     bool noiseActive;
+    int lastSoundValue;
+
+    WebServer server;
+
+    void handleSound();
+    void handleState();
+    void handleJSON();
 };
 
 #endif
