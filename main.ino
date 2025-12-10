30d2e353f1864875feb7d72b60783489d239472d
Farid Chad Abdulsamad <chad.samad@gmail.com>
2025-12-10 15:06:16 -0500

    Updated the main to use the proper files
    Made the main file use the SoundMonitor header as well as start up a server

--- main.ino
+++ main.ino
@@ -1,23 +1,18 @@
-#include <Arduino.h>
-#include "NoiseSensor.h"
+#include "SoundMonitor.h"
 
-NoiseSensor sensor(34); // pin on ESP32
+// Replace with your WiFi credentials
+const char* ssid     = "EDBTZ";
+const char* password = "smilingwind367";
+
+SoundServer soundServer(33, 1200);
 
 void setup() {
     Serial.begin(115200);
-    delay(1000);
-    Serial.println("Starting up");
+    delay(300);
+
+    soundServer.begin(ssid, password);
 }
 
 void loop() {
-    Measurement m = sensor.read("Test Location");
-
-    Serial.print("Timestamp(ms): ");
-    Serial.print(m.timestamp_ms);
-    Serial.print(" | dB: ");
-    Serial.print(m.decibels);
-    Serial.print(" | Location: ");
-    Serial.println(m.location);
-
-    delay(10000); 
+    soundServer.update();
 }
